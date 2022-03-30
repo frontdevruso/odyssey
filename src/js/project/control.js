4fadc0154b858a38a5a98c97457d4c794043f6db
backdevruso <feedback0225@gmail.com>
2022-03-30 10:43:58 +0300

    control full page

--- src/js/project/control.js
+++ src/js/project/control.js
@@ -1,5 +1,52 @@
 const control = () => {
+    const hintSection = document.querySelector('.hints');
 
+    if (hintSection) {
+        $('.hints__message').find('.message__next').on('click', function() {
+            let currentMessage = this.closest('.hints__message');
+            let nextClass = this.getAttribute('data-message');
+            let nextMessage = $(this).parent().siblings(`.${nextClass}`)
+            currentMessage.classList.remove('is-active');
+
+            setTimeout(function() {
+                currentMessage.classList.add('d-none');
+                nextMessage.addClass('is-active');
+            }, 600);
+        });
+    }
+
+    $(".hints__message--2 .message__next").click(function () {
+        $('.page-focus').removeClass('page-focus--center');
+        $('.page-focus').addClass('hidden');
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().top;
+        if ($.browser.safari) {
+            $('body').animate({ scrollTop: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollTop: destination }, 1100);
+        }
+        return false; 
+    });
+
+    $(".hints__message--3 .message__next").click(function () {
+        $('.page__border').removeClass('page__border--hidden');
+        $('.hints').addClass('hints--hidden');
+        $('.hints').addClass('hints--d-none');
+
+        setTimeout(function() {
+            $('.hints').removeClass('hints--hidden');
+            $('.hints').removeClass('hints--d-none');
+        }, 100);
+        
+        let elementClick = $(this).attr("href");
+        let destination = $(elementClick).offset().bottom;
+        if ($.browser.safari) {
+            $('body').animate({ scrollbottom: destination }, 1100); //1100 - скорость
+        } else {
+            $('html').animate({ scrollbottom: destination }, 1100);
+        }
+        return false; 
+    });
 }
 
 export { control };
--- src/js/project/profile-hints.js
+++ src/js/project/profile-hints.js
@@ -8,117 +8,119 @@ const profileHints = () => {
 
     const prizesAchivments = document.querySelectorAll('.prizes-achievement');
 
-    const modalOpen = (modal) => {
-        const content = modal.querySelector('.modal__content');
-        modal.classList.remove('modal--close');
-        modal.classList.add('modal--open');
-        setTimeout(function() {
-            content.classList.add('modal__content--open');
-        }, 1000);
-    }
-
-    const modalClose = (modal) => {
-        const content = modal.querySelector('.modal__content');
-        content.classList.remove('modal__content--open');
-        setTimeout(function() {
-            modal.classList.add('modal--close');
-        }, 400);
-        setTimeout(function() {
-            modal.classList.remove('modal--open');
-        }, 1000);
-    }
-
-    const thirdHintSwitch = () => {
-        document.querySelectorAll('.hints__message').forEach(function(message, index) {
-            if (index <= 1) {
-                message.classList.remove('is-active');
-                message.classList.add('d-none');
-            }
-            if (index === 2) { message.classList.add('is-active') }
-        });
-
-    }
-
-    modalAvatar.querySelector('.modal-close').addEventListener('click', function() {
-        modalClose(modalAvatar);
-        setTimeout(function() {
-            // pageFocus.classList.add('page-focus--leftCenter');
-            // prizesAchivments.forEach(function(item) {
-            //     item.classList.add('focus-priority');
-            // });
-            // hintSection.classList.remove('hints--hidden');
-
-            modalOpen(modalReturnAvatar);
-        }, 1000);
-    });
-
-    modalReturnAvatar.querySelector('.modal__content-offer-btn').addEventListener('click', function() {
-        modalClose(modalReturnAvatar);
-        thirdHintSwitch();
-        setTimeout(function() {
-            pageFocus.classList.add('page-focus--rightCenter');
-            hintSection.classList.remove('hints--hidden');
-            prizesAchivments.forEach(function(item) {
-                item.classList.add('focus-priority');
-            });
-        }, 1000);
-    });
-
-    modalReturnAvatar.querySelector('.modal__content-create-btn').addEventListener('click', function() {
-        modalClose(modalReturnAvatar);
-        setTimeout(function() {
-            modalOpen(modalAvatar);
-        }, 1000);
-    });
-
-    modalAvatar.querySelector('.modal__content-create-btn').addEventListener('click', function() {
-        modalClose(modalAvatar);
-        thirdHintSwitch();
-        setTimeout(function() {
-            pageFocus.classList.add('page-focus--rightCenter');
-            hintSection.classList.remove('hints--hidden');
-            prizesAchivments.forEach(function(item) {
-                item.classList.add('focus-priority');
-            });
-
-        }, 1000);
-    });
-
-    $('.hints__message--4 .message__next').on('click', function() {
-        pageFocus.classList.remove('page-focus--rightCenter');
-        pageFocus.classList.add('hidden');
-        prizesAchivments.forEach(function(item) {
-            item.classList.remove('focus-priority');
-        });
-
-        $('.page__border').removeClass('page__border--hidden');
-        $('.profile__tabs-item-info a').addClass('pulse-anim');
-    });
-
-    if (hintSection) {
-        $('.hints__message').find('.message__next').on('click', function() {
-            let currentMessage = this.closest('.hints__message');
-            let nextClass = this.getAttribute('data-message');
-            let nextMessage = $(this).parent().siblings(`.${nextClass}`)
-            currentMessage.classList.remove('is-active');
-
+    if (document.querySelector('.profile')) {
+        const modalOpen = (modal) => {
+            const content = modal.querySelector('.modal__content');
+            modal.classList.remove('modal--close');
+            modal.classList.add('modal--open');
             setTimeout(function() {
-                currentMessage.classList.add('d-none');
-                nextMessage.addClass('is-active');
-            }, 600);
+                content.classList.add('modal__content--open');
+            }, 1000);
+        }
+    
+        const modalClose = (modal) => {
+            const content = modal.querySelector('.modal__content');
+            content.classList.remove('modal__content--open');
+            setTimeout(function() {
+                modal.classList.add('modal--close');
+            }, 400);
+            setTimeout(function() {
+                modal.classList.remove('modal--open');
+            }, 1000);
+        }
+    
+        const thirdHintSwitch = () => {
+            document.querySelectorAll('.hints__message').forEach(function(message, index) {
+                if (index <= 1) {
+                    message.classList.remove('is-active');
+                    message.classList.add('d-none');
+                }
+                if (index === 2) { message.classList.add('is-active') }
+            });
+    
+        }
+    
+        modalAvatar.querySelector('.modal-close').addEventListener('click', function() {
+            modalClose(modalAvatar);
+            setTimeout(function() {
+                // pageFocus.classList.add('page-focus--leftCenter');
+                // prizesAchivments.forEach(function(item) {
+                //     item.classList.add('focus-priority');
+                // });
+                // hintSection.classList.remove('hints--hidden');
+    
+                modalOpen(modalReturnAvatar);
+            }, 1000);
         });
-    }
-
-    if (createProfile) {
-        const createProfileBtn = document.querySelector('.new-profile-btn');
-        createProfileBtn.addEventListener('click', function() {
-            createProfileBtn.classList.remove('new-profile-btn--animation');
-            createProfile.classList.remove('focus-priority');
-
-            pageFocus.classList.remove('page-focus--left');
-            hintSection.classList.add('hints--hidden');
-            modalOpen(modalAvatar);
+    
+        modalReturnAvatar.querySelector('.modal__content-offer-btn').addEventListener('click', function() {
+            modalClose(modalReturnAvatar);
+            thirdHintSwitch();
+            setTimeout(function() {
+                pageFocus.classList.add('page-focus--rightCenter');
+                hintSection.classList.remove('hints--hidden');
+                prizesAchivments.forEach(function(item) {
+                    item.classList.add('focus-priority');
+                });
+            }, 1000);
         });
+    
+        modalReturnAvatar.querySelector('.modal__content-create-btn').addEventListener('click', function() {
+            modalClose(modalReturnAvatar);
+            setTimeout(function() {
+                modalOpen(modalAvatar);
+            }, 1000);
+        });
+    
+        modalAvatar.querySelector('.modal__content-create-btn').addEventListener('click', function() {
+            modalClose(modalAvatar);
+            thirdHintSwitch();
+            setTimeout(function() {
+                pageFocus.classList.add('page-focus--rightCenter');
+                hintSection.classList.remove('hints--hidden');
+                prizesAchivments.forEach(function(item) {
+                    item.classList.add('focus-priority');
+                });
+    
+            }, 1000);
+        });
+    
+        $('.hints__message--4 .message__next').on('click', function() {
+            pageFocus.classList.remove('page-focus--rightCenter');
+            pageFocus.classList.add('hidden');
+            prizesAchivments.forEach(function(item) {
+                item.classList.remove('focus-priority');
+            });
+    
+            $('.page__border').removeClass('page__border--hidden');
+            $('.profile__tabs-item-info a').addClass('pulse-anim');
+        });
+    
+        if (hintSection) {
+            $('.hints__message').find('.message__next').on('click', function() {
+                let currentMessage = this.closest('.hints__message');
+                let nextClass = this.getAttribute('data-message');
+                let nextMessage = $(this).parent().siblings(`.${nextClass}`)
+                currentMessage.classList.remove('is-active');
+    
+                setTimeout(function() {
+                    currentMessage.classList.add('d-none');
+                    nextMessage.addClass('is-active');
+                }, 600);
+            });
+        }
+    
+        if (createProfile) {
+            const createProfileBtn = document.querySelector('.new-profile-btn');
+            createProfileBtn.addEventListener('click', function() {
+                createProfileBtn.classList.remove('new-profile-btn--animation');
+                createProfile.classList.remove('focus-priority');
+    
+                pageFocus.classList.remove('page-focus--left');
+                hintSection.classList.add('hints--hidden');
+                modalOpen(modalAvatar);
+            });
+        }
     }
 }
 
